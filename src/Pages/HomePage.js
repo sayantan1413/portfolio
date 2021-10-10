93b705f7e7d9ebcb686355ec54cf2a86b46785f7
sayantan1413 <sayantanbose2001@gmail.com>
2021-10-11 01:46:00 +0530

    Styled home page
    Signed-off-by: sayantan1413 <sayantanbose2001@gmail.com>

---
+++ src/Pages/HomePage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components';
+function HomePage() {
+    return (
+        <HomePageStyled>
+
+        </HomePageStyled>
+    )
+}
+
+const HomePageStyled = styled.header`
+    width: 100%;
+    height: 100vh;
+
+`;
+
+export default HomePage
