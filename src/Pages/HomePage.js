ee9e952638dda1c7c42b756cefd3f8cd758bc6e2
sayantan1413 <sayantanbose2001@gmail.com>
2021-10-13 16:03:34 +0530

    Added info and styled the homepage
    Signed-off-by: sayantan1413 <sayantanbose2001@gmail.com>

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,11 +1,25 @@
 import React from 'react'
 import styled from 'styled-components';
 import Particles from 'react-particles-js';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import GitHubIcon from '@mui/icons-material/GitHub';
 function HomePage() {
     return (
         <HomePageStyled>
             <div className="p-particles-js">
                 <Particles />
+            </div>
+            <div className="typography">
+                <h1>Hi I'm <span>Sayantan Bose</span></h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                <div className="icons">
+                    <a href="#" className="icon i-facebook"><FacebookIcon /></a>
+                    <a href="#" className="icon i-linkedin"><LinkedInIcon /></a>
+                    <a href="#" className="icon i-twitter"><TwitterIcon /></a>
+                    <a href="#" className="icon i-github"><GitHubIcon /></a>
+                </div>
             </div>
         </HomePageStyled>
     )
@@ -19,6 +33,42 @@ const HomePageStyled = styled.header`
     .p-particles-js {
         position: absolute;
         top: 0;
+        left: 0;
+    }
+
+    .typography {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        text-align: center;
+        width: 80%;
+    }
+
+    .icons {
+        display: flex;
+        justify-content: center;
+        margin-top: 1rem;
+        
+        .icon {
+            border: 2px solid var(--border-color);
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            border-radius: 50%;
+            transition: all 0.4s ease-in-out;
+            cursor: pointer;
+            &:hover{
+            border: 2px solid var(--primary-color);
+            color: var(--primary-color);
+            }
+            &:not(:last-child){
+                margin-right: 1rem;
+            }
+            svg {
+                margin: 0.5rem;
+            }
+        }
     }
 `;
 
--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -39,6 +39,19 @@ const GlobalStyled = createGlobalStyle`
         color: inherit;
         font-size: inherit;
     }
+
+    h1 {
+        font-size: 4rem;
+        color: var(--white-color)
+        span{
+            font-size: 4rem;
+        }
+    }
+
+    span{
+        font-size: 4rem;
+        color: var(--primary-color);
+    }
 `;
 
 export default GlobalStyled;
