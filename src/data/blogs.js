88d98cf2804c09186a6cebb14b72eabb97a455df
sayantan1413 <sayantanbose2001@gmail.com>
2021-10-18 00:12:11 +0530

    Styled and added the blog section
    Signed-off-by: sayantan1413 <sayantanbose2001@gmail.com>

---
+++ src/data/blogs.js
@@ -0,0 +1,38 @@
+import blog1 from  '../images/blogs/blog1.svg';
+import blog3 from  '../images/blogs/blog3.svg';
+import blog2 from  '../images/blogs/blog2.svg';
+import blog4 from  '../images/blogs/blog4.svg';
+
+const blogs = [
+    {
+        id: 2,
+        title: 'How To Use SEO Efficiently',
+        date: '01',
+        month: 'April',
+        image: blog2
+    },
+    {
+        id: 3,
+        title: 'How to choose a programming Language',
+        date: '01',
+        month: 'April',
+        image: blog3,
+        link: 'https://www.google.co.uk/'
+    },
+    {
+        id: 4,
+        title: 'How To Tse SEO Efficiently',
+        date: '01',
+        month: 'April',
+        image: blog4
+    },
+    {
+        id: 5,
+        title: 'How To Tse SEO Efficiently',
+        date: '01',
+        month: 'April',
+        image: blog4
+    }
+]
+
+export default blogs;
